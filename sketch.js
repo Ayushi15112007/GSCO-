0b743dac953321be14ce0e9071e1dc2226a64df8
Ayushi15112007 <79367377+Ayushi15112007@users.noreply.github.com>
2021-03-18 20:34:12 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ function setup() {
   createCanvas(1600,400);
 
  car= createSprite(50,200,50,50);
- wall= createSprite(1500,200,60,height/2);
+ wall= createSprite(1300,200,60,height/2);
 
 speed= random(55,90);
 weight= random(400,1500);
